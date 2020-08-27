4ea601faca73bdb13831faa97c8cc329b5c64114
sid-0309 <sid-0309@users.noreply.github.com>
2020-08-27 17:14:10 +0530

    mostly done

--- auto.py
+++ auto.py
@@ -1,26 +1,42 @@
-import datetime
 import os
 import img2pdf
-from rename import rename
+import shutil
+dir = os.getcwd()
 
-def check(a):
-    try:
-        int(a)
-        return True
-    except ValueError:return False
-def correct(d):
-    rename(d)
-    name_lst = [i for i in os.listdir(d) if i.endswith(".png")]
-name = input("filename: ")
-d = input("dir: ")
-if d == "":d = str(datetime.date.today())
-if name == "": name = "final__"+str(datetime.date.today())
-#ext = [".jpg", ".png", ".jpeg", ".bmp"]
-name_lst = [i for i in os.listdir(d) if i.endswith(".png")]
-for i in name_lst:
-    part = i.partition(".")
-    if check(part[0]) is True:
-        continue
-    else:correct(d)
-with open(name+".pdf", "wb") as f:
-    f.write(img2pdf.convert([d+"\\"+i for i in name_lst]))
+def rename(d, ext = ".jpg"):
+    os.chdir(d)
+    max = len(str(len([i for i in os.listdir() if i.endswith(ext)])))
+    for i in os.listdir():
+        print(i)
+        print(os.getcwd())
+        name = ""
+        if not i.endswith(ext):continue
+        part=i.partition(".")
+        for j in part[0]:
+            if j.isdigit():name+=j
+        if len(name) < max: name = "0"*(max-len(name))+name
+        os.rename(i, name+part[1]+part[2])
+
+    os.chdir(dir)
+
+def pdf(d,name, ext = ".jpg"):
+    os.chdir(d)
+    with open(name+".pdf", "wb") as f:
+        f.write(img2pdf.convert([i for i in os.listdir() if i.endswith(ext)]))
+    shutil.move(d+"\\"+name+".pdf", dir+"\\"+name+".pdf")
+
+def main():
+    '''
+    folder = input("dir: ")
+    pdfname = input("filename: ")
+    ext = input("extention: ")
+    '''
+    folder = "E:\org2"
+    pdfname = "test"
+    ext = ".png"
+
+    rename(folder) if ext == "" else rename(folder, ext)
+
+    pdf(folder, pdfname) if ext == "" else pdf(folder, pdfname,ext)
+
+main()
